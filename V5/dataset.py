24dd0613414a5231fc3dd8f2b97f3558de901175
yuemengrui <yuemengrui@aistreamrpa.com>
2022-07-20 16:01:56 +0800

    V5

---
+++ V5/dataset.py
@@ -0,0 +1,216 @@
+# *_*coding:utf-8 *_*
+# @Author : YueMengRui
+import os
+import numpy as np
+from torch.utils.data import Dataset
+import cv2
+import json
+import torch
+import random
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+
+def iou(box1, box2):
+    """
+    两个框（二维）的 iou 计算
+    注意：边框以左上为原点
+    box:[x1,y1,x2,y2],依次为左上右下坐标
+    """
+
+    h = max(0, min(box1[2], box2[2]) - max(box1[0], box2[0]))
+    w = max(0, min(box1[3], box2[3]) - max(box1[1], box2[1]))
+    area_box1 = ((box1[2] - box1[0]) * (box1[3] - box1[1]))
+    area_box2 = ((box2[2] - box2[0]) * (box2[3] - box2[1]))
+    inter = w * h
+    union = area_box1 + area_box2 - inter
+    iou = inter / union
+    return iou
+
+
+def crop_ok(target_box, boxes, threshold=0.5):
+    for box in boxes:
+        if box[0] > target_box[2]:
+            continue
+
+        if box[2] < target_box[0]:
+            continue
+
+        if box[1] > target_box[3]:
+            break
+
+        if iou(target_box, box) > threshold:
+            return True
+
+    return False
+
+
+class ImagePadding(object):
+
+    def __init__(self, rate=0.5):
+        """
+        :param rate: rate = h / w
+        """
+        self.rate = rate
+
+    def __call__(self, img):
+        ori_h, ori_w = img.shape[:2]
+
+        if ori_h / ori_w > 0.5:
+            new_w = ori_h * 2
+            img = cv2.copyMakeBorder(img, 0, 0, 0, new_w - ori_w, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+        elif ori_h / ori_w < 0.5:
+            new_h = int(ori_w / 2)
+            img = cv2.copyMakeBorder(img, 0, new_h - ori_h, 0, 0, cv2.BORDER_CONSTANT, value=(0, 0, 0))
+
+        return img
+
+
+class ImageResize(object):
+
+    def __init__(self, size=(1024, 512)):
+        self.size = size
+
+    def __call__(self, img, **kwargs):
+        h = img.shape[0]
+        scale = self.size[1] / h
+        img = cv2.resize(img, self.size)
+
+        return img, scale
+
+
+class ViTSegDataset(Dataset):
+
+    def __init__(self, dataset_dir, mode='train', target_w_range=(30, 1000), target_h_range=(20, 350),
+                 target_w_h_rate=(0.7, 20), threshold=0.6, **kwargs):
+
+        self.target_w_range = target_w_range
+        self.target_h_range = target_h_range
+        self.target_w_h_rate = target_w_h_rate
+        self.threshold = threshold
+
+        self.image_padding = ImagePadding()
+        self.image_resize = ImageResize()
+        self.target_resize = ImageResize(size=(64, 32))
+
+        self.transform = transforms.Compose([transforms.ToTensor(),
+                                             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+                                             ])
+
+        self.data_list = self._load_data(dataset_dir, mode)
+
+    def _load_data(self, dataset_dir, mode='train'):
+        img_dir = os.path.join(dataset_dir, 'images')
+        boxes_dir = os.path.join(dataset_dir, 'box_labels')
+
+        file_list = os.listdir(img_dir)
+
+        data_list = []
+        for fil in file_list:
+            file_name = fil.split('.')[0]
+            img_path = os.path.join(img_dir, fil)
+            box_path = os.path.join(boxes_dir, file_name + '.json')
+
+            data_list.append({'img_path': img_path, 'box_path': box_path})
+
+        if mode == 'val':
+            random.shuffle(data_list)
+            return data_list[:50]
+        return data_list
+
+    def get_crop_img(self, ori_h, ori_w, boxes):
+        while True:
+            x1 = random.randint(0, ori_w - self.target_w_range[0])
+            y1 = random.randint(0, ori_h - self.target_h_range[0])
+            target_w = random.randint(self.target_w_range[0], self.target_w_range[1])
+            target_h = random.randint(self.target_h_range[0], self.target_h_range[1])
+
+            x2 = min(ori_w, x1 + target_w)
+            y2 = min(ori_h, y1 + target_h)
+
+            w = x2 - x1
+            h = y2 - y1
+            rate = w / h
+            if self.target_w_h_rate[0] <= rate <= self.target_w_h_rate[1]:
+                if crop_ok([x1, y1, x2, y2], boxes, self.threshold):
+                    return [x1, y1, x2, y2]
+
+    def __getitem__(self, idx):
+        try:
+            data = self.data_list[idx]
+
+            img = cv2.imread(data['img_path'])
+            with open(data['box_path'], 'r', encoding='utf8') as f:
+                boxes = json.load(f)
+
+            ori_h, ori_w = img.shape[:2]
+
+            label = np.uint8(np.zeros((ori_h, ori_w)))
+
+            target_box = self.get_crop_img(ori_h, ori_w, boxes)
+
+            target = img[target_box[1]:target_box[3], target_box[0]:target_box[2]]
+
+            label[target_box[1]:target_box[3], target_box[0]:target_box[2]] = 1
+
+            img = self.image_padding(img)
+            img, _ = self.image_resize(img)
+            img = self.transform(img)
+
+            target = self.image_padding(target)
+            target, _ = self.target_resize(target)
+            target = self.transform(target)
+
+            label = self.image_padding(label)
+            label, _ = self.image_resize(label)
+
+            label = torch.from_numpy(label)
+
+            return img, target, label
+
+        except Exception as e:
+            print(e)
+            return self.__getitem__(np.random.randint(self.__len__()))
+
+    def __len__(self):
+        return len(self.data_list)
+
+# if __name__ == '__main__':
+#     dataset = ViTSegDataset(dataset_dir='/Users/yuemengrui/Data/RPAUI/web_cv_data')
+#     rate = 0
+#     start = time.time()
+#     for i in range(100):
+#         img, target, label = dataset[i]
+#     print(time.time()-start)
+#     #     dataset = ViTDetDataset(dataset_dir='/Users/yuemengrui/Data/RPAUI/web_cv_data')
+#     #     train_loader = DataLoader(dataset, batch_size=2, shuffle=True, drop_last=True)
+#
+#     # for img, target, label in train_loader:
+#     #     print(img.shape)
+#     #     print(target.shape)
+#     #     print(label.shape)
+#     #     break
+#     #         print(label)
+#     #         print(center_range.numpy().tolist())
+#     #         center_range = center_range.numpy().tolist()
+#     #         preds = [[0.508238673210144, 0.4653865396976471, 0.49629437923431396], [0.2,0.2,0.2]]
+#     #
+#     #         for i in range(len(preds)):
+#     #             print("==========================")
+#     #             print(preds[i])
+#     #             print(center_range[i])
+#     #
+#     #             pred_c_x = preds[i][0]
+#     #             pred_c_y = preds[i][1]
+#     #
+#     #             print(pred_c_x, pred_c_y)
+#     #
+#     #             label_c_x_min = center_range[i][0]
+#     #             label_c_x_max = center_range[i][1]
+#     #             label_c_y_min = center_range[i][2]
+#     #             label_c_y_max = center_range[i][3]
+#     #
+#     #             print(label_c_x_min, label_c_x_max)
+#     #             print(label_c_y_min, label_c_y_max)
+#     #
+#     #
---
+++ V5/model.py
@@ -0,0 +1,435 @@
+# *_*coding:utf-8 *_*
+# @Author : YueMengRui
+import torch
+from torch import nn
+import torch.nn.functional as F
+from einops import rearrange, repeat
+from torch import Tensor
+from einops.layers.torch import Rearrange
+from typing import Type, Any, Callable, Union, List, Optional
+
+
+def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+
+def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion: int = 1
+
+    def __init__(
+            self,
+            inplanes: int,
+            planes: int,
+            stride: int = 1,
+            downsample: Optional[nn.Module] = None,
+            groups: int = 1,
+            base_width: int = 64,
+            dilation: int = 1,
+            norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(BasicBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion: int = 4
+
+    def __init__(
+            self,
+            inplanes: int,
+            planes: int,
+            stride: int = 1,
+            downsample: Optional[nn.Module] = None,
+            groups: int = 1,
+            base_width: int = 64,
+            dilation: int = 1,
+            norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(Bottleneck, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(
+            self,
+            block: Type[Union[BasicBlock, Bottleneck]],
+            layers: List[int],
+            zero_init_residual: bool = False,
+            groups: int = 1,
+            width_per_group: int = 64,
+            replace_stride_with_dilation: Optional[List[bool]] = None,
+            norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+
+        self.last_conv = nn.Conv2d(2048, 512, 1)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
+
+    def _make_layer(self, block: Type[Union[BasicBlock, Bottleneck]], planes: int, blocks: int,
+                    stride: int = 1, dilate: bool = False) -> nn.Sequential:
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        print('resnet: ', x.shape)
+
+        # x = self.last_conv(x)
+        # print('resnet: ', x.shape)
+        return x
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
+
+
+def _resnet(
+        block: Type[Union[BasicBlock, Bottleneck]],
+        layers: List[int],
+        **kwargs: Any
+) -> ResNet:
+    model = ResNet(block, layers, **kwargs)
+    return model
+
+
+def resnet18(**kwargs: Any) -> ResNet:
+    return _resnet(BasicBlock, [2, 2, 2, 2], **kwargs)
+
+
+def resnet50(**kwargs: Any) -> ResNet:
+    return _resnet(Bottleneck, [3, 4, 6, 3], **kwargs)
+
+
+def pair(t):
+    return t if isinstance(t, tuple) else (t, t)
+
+
+class MatchHead(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Conv2d(512, 2, kernel_size=1, stride=1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = F.interpolate(x, scale_factor=32, mode='bilinear', align_corners=True)
+        # x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
+        # x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
+        # x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
+        # x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
+        return x
+
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim, dropout=0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.attend = nn.Softmax(dim=-1)
+        self.dropout = nn.Dropout(dropout)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+        attn = self.dropout(attn)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
+            ]))
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
+
+
+class ViT(nn.Module):
+
+    def __init__(self, image_size=(512, 1024), patch_size=(32, 64), dim=512, depth=6, heads=8, mlp_dim=512, channels=3,
+                 dim_head=64, dropout=0., emb_dropout=0., pool='cls'):
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height % patch_height == 0 and image_width % patch_width == 0, 'Image dimensions must be divisible by the patch size.'
+
+        num_patches = (image_height // patch_height) * (image_width // patch_width)
+
+        patch_dim = channels * patch_height * patch_width
+
+        assert pool in {'cls', 'mean'}, 'pool type must be either cls (cls token) or mean (mean pooling)'
+
+        # self.to_patch_embedding = nn.Sequential(
+        #     Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_height, p2=patch_width),
+        #     nn.Linear(patch_dim, dim)
+        # )
+        # self.to_patch_embedding = nn.Unfold(kernel_size=patch_size, stride=patch_size)
+        # self.patch_weight = nn.Parameter(torch.randn(patch_dim, dim))
+        self.to_image_embedding = nn.Sequential(
+            resnet18(),
+            Rearrange('b c h w -> b (h w) c')
+        )
+
+        self.to_target_embedding = nn.Sequential(
+            Rearrange('b c (pn1 h) (pn2 w) -> b (pn1 pn2) (h w c)', pn1=1, pn2=1),
+            nn.Linear(patch_dim, dim)
+        )
+
+        self.pos_embedding = nn.Parameter(torch.randn(1, 512 + 1, dim))
+        # self.pos_embedding = nn.Parameter(torch.randn(1, num_patches, dim))
+
+        # self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
+
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+
+        self.pool = pool
+
+        self.l = nn.Linear(513, 512)
+
+        self.to_cnn = nn.Sequential(
+            Rearrange('b (h w) c -> b c h w', h=16, w=32)
+        )
+
+        self.match_head = MatchHead()
+
+    def forward(self, img, target_img=None):
+        x = self.to_image_embedding(img)  # [N, 512, 512]
+
+        target = self.to_target_embedding(target_img)  # [N, 1, 512]
+
+        b, n, _ = x.shape
+        # cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b=b)
+        #
+        x = torch.cat((target, x), dim=1)  # [N, 513, 512]
+
+        #
+        # target = torch.cat((cls_tokens, target), dim=1)
+
+        x += self.pos_embedding[:, :(n + 1)]  # [N, 513, 512]
+
+        x = self.dropout(x)
+
+        x = self.transformer(x)  # [N, 513, 512]
+
+        x = self.l(x.permute(0, 2, 1))  # [N, 512, 512]
+
+        x = self.to_cnn(x)  # [N, 512, 16, 32]
+
+        x = self.match_head(x)
+
+        return x
+
+
+if __name__ == '__main__':
+    model = ViT(patch_size=(32, 64), dim=512, depth=6, heads=8, mlp_dim=512)
+
+    img = torch.randn((1, 3, 512, 1024))
+
+    target_img = torch.randn((1, 3, 32, 64))
+
+    out = model(img, target_img)
+    print(out.shape)
+    # print(out)
