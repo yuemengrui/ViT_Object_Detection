a169a8622e67a7d03a99dbd254a6ad8d3adbd92f
yuemengrui <1401190335@qq.com>
2022-07-22 22:36:06 +0800

    V5 target_conv

--- V5/model_target_conv.py
+++ V5/model_target_conv.py
@@ -4,9 +4,8 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from einops import rearrange, repeat
-from torch import Tensor
 from einops.layers.torch import Rearrange
-from typing import Type, Any, Callable, Union, List, Optional
+from copy import deepcopy
 
 
 def pair(t):
@@ -139,11 +138,16 @@ class ViT(nn.Module):
 
         # self.dropout = nn.Dropout(emb_dropout)
 
+        self.to_target_feature = nn.Sequential(
+            Rearrange('b c h w -> b c (h w )'),
+            nn.Linear(481 * 961, dim)
+        )
+
         self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
 
         self.pool = pool
 
-        self.l = nn.Linear(257, 256)
+        self.l = nn.Linear(258, 256)
 
         self.to_up = nn.Sequential(
             nn.Linear(dim, patch_dim),
@@ -157,10 +161,10 @@ class ViT(nn.Module):
         self.match_head = MatchHead()
 
     def forward(self, img, target_img):
-        x = self.to_patch_embedding(img).transpose(-1, -2)
+        x = self.to_patch_embedding(deepcopy(img)).transpose(-1, -2)
         x = x @ self.patch_weight  # [N, 256, 512]
 
-        target = self.to_target_embedding(target_img)  # [N, 1, 512]
+        target = self.to_target_embedding(deepcopy(target_img))  # [N, 1, 512]
 
         b, n, _ = x.shape
         # cls_tokens = repeat(self.cls_token, '1 1 d -> b 1 d', b=b)
@@ -174,7 +178,13 @@ class ViT(nn.Module):
 
         # x = self.dropout(x)
 
-        x = self.transformer(x)  # [N, 257, 512]
+        feature = F.conv2d(img, target_img)
+
+        feature = self.to_target_feature(feature)
+
+        x = torch.cat((feature, x), dim=1)  # [N, 258, 512]
+
+        x = self.transformer(x)  # [N, 258, 512]
 
         x = self.l(x.permute(0, 2, 1)).permute(0, 2, 1)  # [N, 256, 512]
 
